4228fb39c2d14df48c07eece1abbd64c5d1819d8
TharindaDilshan <tharindad7@gmail.com>
2020-12-09 16:33:13 +0530

    Optimize toml file update

--- lib/ballerina_dependabot/file_updater.py
+++ lib/ballerina_dependabot/file_updater.py
@@ -1,26 +1,70 @@
 import file_fetcher
 import helpers
+import toml
 
 def updateFileAndRaisePR(repo, modulesToBeUpdated):
     for module in modulesToBeUpdated:
         latestVersion = modulesToBeUpdated[module]
         tomlFile = file_fetcher.fetchTomlFileFromMainOrExistingBranch(repo, module)
         modifiedTomlFile, currentVersion, commitFlag = updatePropertiesFile(tomlFile, module, latestVersion)
+        if commitFlag:
+            commitChanges(modifiedTomlFile, currentVersion, repo, module['name'], module['version'])
+            createPullRequest(repo, currentVersion, module['name'], module['version'])
 
 def updatePropertiesFile(tomlFile, module, latestVersion):
     modifiedTomlFile = ''
     commitFlag = False
 
+    currentVersion = toml.loads(tomlFile)['dependencies'][module]
+    # update only if the current version < latest version
+    isCurrentVersionLatest = helpers.compareVersion(latestVersion, currentVersion)
+
     for line in tomlFile.splitlines():
-        if module in line:
-            currentVersion = line.split('=')[-1].replace(" ","")[1:-1]
-            # update only if the current version < latest version
-            if helpers.compareVersion(latestVersion, currentVersion) == 1:
-                update = line.split('=')[0] + '= "' + latestVersion '"\n'
-                modifiedTomlFile += update
-                commitFlag = True
+        if module in line and isCurrentVersionLatest == 1:
+            update = line.split('=')[0] + '= "' + latestVersion '"\n'
+            modifiedTomlFile += update
+            commitFlag = True
         else:
             update = line + '\n'
             modifiedTomlFile += update
     
     return modifiedTomlFile, currentVersion, commitFlag
+
+# Checkout branch and commit changes
+def commitChanges(data, currentVersion, repo, module, latestVersion):
+    author = InputGitAuthor(packageUser, packageEmail)
+
+    # If branch already exists checkout and commit else create new branch from master branch and commit
+    try:
+        source = repo.get_branch(branch="dependabot-" + module)
+    except GithubException:
+        source = repo.get_branch("master")
+        repo.create_git_ref(ref=f"refs/heads/dependabot-" + module, sha=source.commit.sha)
+
+    contents = repo.get_contents("gradle.properties", ref="dependabot-" + module)
+    repo.update_file(contents.path, 
+                    "[Automated] Bump " + module + " from " + currentVersion + " to " + latestVersion, 
+                    data, 
+                    contents.sha, 
+                    branch="dependabot-" + module, 
+                    author=author)
+
+# Create a PR from the branch created
+def createPullRequest(repo, currentVersion, module, latestVersion):
+    pulls = repo.get_pulls(state='open', head='dependabot-' + module, base='master')
+    PRExists = 0
+
+    # Check if a PR already exists for the module
+    for pull in pulls:
+        if module in pull.title:
+            PRExists = pull.number
+
+    # If PR exists update the title else create a new PR
+    if PRExists:
+        existingPR = repo.get_pull(PRExists)
+        existingPR.edit(title="Bump " + module + " from " + currentVersion + " to " + latestVersion)
+    else:
+        repo.create_pull(title="Bump " + module + " from " + currentVersion + " to " + latestVersion, 
+                        body='$subject', 
+                        head="dependabot-" + module, 
+                        base="master")
