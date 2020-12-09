26c10d9bcf858b1cbe12ca4656b9dbca0f688940
TharindaDilshan <tharindad7@gmail.com>
2020-12-09 15:31:58 +0530

    Add function to update toml file with latest version

--- lib/ballerina_dependabot/file_updater.py
+++ lib/ballerina_dependabot/file_updater.py
@@ -1,4 +1,5 @@
 import file_fetcher
+import helpers
 
 def updateFileAndRaisePR(repo, modulesToBeUpdated):
     for module in modulesToBeUpdated:
@@ -7,4 +8,19 @@ def updateFileAndRaisePR(repo, modulesToBeUpdated):
         modifiedTomlFile, currentVersion, commitFlag = updatePropertiesFile(tomlFile, module, latestVersion)
 
 def updatePropertiesFile(tomlFile, module, latestVersion):
+    modifiedTomlFile = ''
+    commitFlag = False
+
+    for line in tomlFile.splitlines():
+        if module in line:
+            currentVersion = line.split('=')[-1].replace(" ","")[1:-1]
+            # update only if the current version < latest version
+            if helpers.compareVersion(latestVersion, currentVersion) == 1:
+                update = line.split('=')[0] + '= "' + latestVersion '"\n'
+                modifiedTomlFile += update
+                commitFlag = True
+        else:
+            update = line + '\n'
+            modifiedTomlFile += update
     
+    return modifiedTomlFile, currentVersion, commitFlag
