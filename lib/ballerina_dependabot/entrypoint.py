b1a04262ed500290a2879c1394218e11d1a0bea7
TharindaDilshan <tharindad7@gmail.com>
2020-12-09 20:41:03 +0530

    Print working directory

--- lib/ballerina_dependabot/entrypoint.py
+++ lib/ballerina_dependabot/entrypoint.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import os
-from commons import configureGithubRepository
-from file_fetcher import fetchTomlFileFromMainBranch
-from file_parser import getModulesToBeUpdated
-from file_updater import updateFileAndRaisePR
+# from commons import configureGithubRepository
+# from file_fetcher import fetchTomlFileFromMainBranch
+# from file_parser import getModulesToBeUpdated
+# from file_updater import updateFileAndRaisePR
 
-repo = configureGithubRepository()
-tomlFile = file_fetcher.fetchTomlFileFromMainBranch(repo)
-modulesToBeUpdated = file_parser.getModulesToBeUpdated(tomlFile)
+# repo = configureGithubRepository()
+# tomlFile = file_fetcher.fetchTomlFileFromMainBranch(repo)
+# modulesToBeUpdated = file_parser.getModulesToBeUpdated(tomlFile)
 
-if len(modulesToBeUpdated) > 0:
-    file_updater.updateFileAndRaisePR(repo, modulesToBeUpdated)
+# if len(modulesToBeUpdated) > 0:
+#     file_updater.updateFileAndRaisePR(repo, modulesToBeUpdated)
 
+cwd = os.getcwd()
+print(cwd)
 exit(0)
